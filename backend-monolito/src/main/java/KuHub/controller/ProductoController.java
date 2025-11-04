ea9a5a3cb0848cd60612fc3205947557a2f72d55
Matheus Lara <ma.delara@duocuc.cl>
2025-11-04 00:07:43 -0300

    Prueba con monolito, producto conectado a bbdd

---
+++ backend-monolito/src/main/java/KuHub/controller/ProductoController.java
@@ -0,0 +1,38 @@
+package KuHub.controller;
+
+import KuHub.entity.Producto;
+import KuHub.service.ProductoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/producto")
+@Validated
+public class ProductoController {
+
+    @Autowired
+    private ProductoService productoService;
+
+    //Metodos creado solamente para acceder los productos por client rest para la solicitude.
+
+    @GetMapping
+    public ResponseEntity<List<Producto>> findAllProductos(){
+        return ResponseEntity
+                .status(200)
+                .body(productoService.findAll());
+    }
+
+    @GetMapping("/id/{id}")
+    public ResponseEntity<Producto> findProductoById(@PathVariable Long id){
+        return ResponseEntity
+                .status(200)
+                .body(productoService.findById(id));
+    }
+}
---
+++ backend-monolito/src/main/java/KuHub/entity/Producto.java
@@ -0,0 +1,52 @@
+package KuHub.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Entity
+@Table(name ="producto")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_producto",nullable = false)
+    private Long idProducto;
+
+    @Column(name="cod_producto",unique = true)
+    private String codProducto;
+
+    @Column(name="nombre_producto",nullable = false,unique = true)
+    @NotBlank(message = "El campo nombre del producto no puede ser vacio")
+    private String nombreProducto;
+
+    @Column(name="categoria",nullable = false)
+    @NotBlank(message = "El campo categoria no puede ser vacio")
+    private String categoria;
+
+    @Column(name="unidad_medida" , nullable = false)
+    @NotBlank(message = "El campo unidad medida no puede ser vacio")
+    private String unidadMedida;
+
+    @Column(name="activo",nullable = false)
+    private Boolean activo = true;
+
+    @Column(name="foto_producto")
+    private byte[] fotoProducto;
+
+    /**-- Tabla producto
+     CREATE TABLE producto (
+     id_producto SERIAL PRIMARY KEY,
+     cod_producto VARCHAR(30),
+     nombre_producto VARCHAR(100) NOT NULL,
+     categoria VARCHAR(100) NOT NULL,
+     unidad_medida VARCHAR(50) NOT NULL,
+     activo BOOLEAN DEFAULT TRUE, --soft delete
+     foto_producto BYTEA
+     );*/
+}
